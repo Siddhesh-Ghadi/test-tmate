7726e3f2619cf18ecdb53c929584f77bbbe144ff
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-09-01 11:45:09 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 __install_ant() {
   echo "installing ant"
-  mkdir -p /opt/ant
-  curl -sL https://downloads.apache.org//ant/binaries/apache-ant-1.10.11-bin.tar.gz | tar -xz --strip 1 -C /opt/ant
-  echo 'export ANT_HOME=/opt/ant
-  export PATH=${ANT_HOME}/bin:${PATH}' > /home/travis/.bash_profile.d/ant.bash
-  chmod 644 /home/travis/.bash_profile.d/ant.bash
-  chown travis: /home/travis/.bash_profile.d/ant.bash
-  source /home/travis/.bash_profile.d/ant.bash
-  cat /home/travis/.bash_profile.d/ant.bash
+  curl -sL https://downloads.apache.org//ant/binaries/apache-ant-1.10.11-bin.tar.gz | tar -xf
+  apache-ant-1.10.11/bin/ant
 }
 __install_ant
