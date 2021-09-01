e011a65ee0e774fb6b611f9fda566c414a5bd78f
Rohan-Shinde-98 <82387052+Rohan-Shinde-98@users.noreply.github.com>
2021-09-01 11:50:10 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -2,7 +2,6 @@
 __install_ant() {
   echo "installing ant"
   curl -sL https://downloads.apache.org//ant/binaries/apache-ant-1.10.11-bin.tar.gz | tar -xz
-  ls *
   apache-ant-1.10.11/bin/ant
 }
 __install_ant
